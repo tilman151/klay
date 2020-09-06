0231491c26c0711e267ae062058610ceb203238c
Tilman Krokotsch <t.krokotsch@gmail.com>
2020-09-06 19:12:26 +0200

    Add SaturnClassifier example.

---
+++ src/main/kotlin/org/klay/examples/SaturnClassifier.kt
@@ -0,0 +1,110 @@
+package org.klay.examples
+
+import org.datavec.api.records.reader.RecordReader
+import org.datavec.api.records.reader.impl.csv.CSVRecordReader
+import org.datavec.api.split.FileSplit
+import org.deeplearning4j.datasets.datavec.RecordReaderDataSetIterator
+import org.deeplearning4j.nn.multilayer.MultiLayerNetwork
+import org.deeplearning4j.nn.weights.WeightInit
+import org.deeplearning4j.optimize.listeners.ScoreIterationListener
+import org.klay.examples.utils.DownloaderUtility
+import org.klay.examples.utils.PlotUtil.generatePointsOnGraph
+import org.klay.examples.utils.PlotUtil.plotTestData
+import org.klay.examples.utils.PlotUtil.plotTrainingData
+import org.nd4j.evaluation.classification.Evaluation
+import org.nd4j.linalg.activations.Activation
+import org.nd4j.linalg.dataset.api.iterator.DataSetIterator
+import org.nd4j.linalg.learning.config.Nesterovs
+import org.nd4j.linalg.lossfunctions.LossFunctions.LossFunction
+import java.io.File
+import java.util.concurrent.TimeUnit
+import org.klay.nn.*
+
+
+/**
+ * "Saturn" Data Classification Example
+ *
+ * Based on the data from Jason Baldridge:
+ * https://github.com/jasonbaldridge/try-tf/tree/master/simdata
+ *
+ * @author Josh Patterson
+ * @author Alex Black (added plots)
+ */
+object SaturnClassifier {
+    var dataLocalPath: String? = null
+    var visualize = true
+    @Throws(Exception::class)
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val batchSize = 50
+        val seed = 123
+        val learningRate = 0.005
+        //Number of epochs (full passes of the data)
+        val nEpochs = 30
+        val numInputs = 2
+        val numOutputs = 2
+        val numHiddenNodes = 20
+        dataLocalPath = DownloaderUtility.CLASSIFICATIONDATA.Download()
+        //Load the training data:
+        val rr: RecordReader = CSVRecordReader()
+        rr.initialize(FileSplit(File(dataLocalPath, "saturn_data_train.csv")))
+        val trainIter: DataSetIterator = RecordReaderDataSetIterator(rr, batchSize, 0, 2)
+
+        //Load the test/evaluation data:
+        val rrTest: RecordReader = CSVRecordReader()
+        rrTest.initialize(FileSplit(File(dataLocalPath, "saturn_data_eval.csv")))
+        val testIter: DataSetIterator = RecordReaderDataSetIterator(rrTest, batchSize, 0, 2)
+
+        //log.info("Build model....");
+        val conf = sequential {
+            seed(seed.toLong())
+            weightInit(WeightInit.XAVIER)
+            updater(Nesterovs(learningRate, 0.9))
+            layers {
+                dense {
+                    nIn(numInputs)
+                    nOut(numHiddenNodes)
+                    activation(Activation.RELU)
+                }
+                output {
+                    lossFunction(LossFunction.NEGATIVELOGLIKELIHOOD)
+                    activation(Activation.SOFTMAX)
+                    nIn(numHiddenNodes)
+                    nOut(numOutputs)
+                }
+            }
+        }
+        val model = MultiLayerNetwork(conf)
+        model.init()
+        model.setListeners(ScoreIterationListener(10)) //Print score every 10 parameter updates
+        model.fit(trainIter, nEpochs)
+        println("Evaluate model....")
+        val eval = model.evaluate<Evaluation>(testIter)
+        println(eval.stats())
+        println("\n****************Example finished********************")
+
+        //Training is complete. Code that follows is for plotting the data & predictions only
+        generateVisuals(model, trainIter, testIter)
+    }
+
+    @Throws(Exception::class)
+    fun generateVisuals(model: MultiLayerNetwork?, trainIter: DataSetIterator?, testIter: DataSetIterator?) {
+        if (visualize) {
+            val xMin = -15.0
+            val xMax = 15.0
+            val yMin = -15.0
+            val yMax = 15.0
+
+            //Let's evaluate the predictions at every point in the x/y input space, and plot this in the background
+            val nPointsPerAxis = 100
+
+            //Generate x,y points that span the whole range of features
+            val allXYPoints = generatePointsOnGraph(xMin, xMax, yMin, yMax, nPointsPerAxis)
+            //Get train data and plot with predictions
+            plotTrainingData(model!!, trainIter!!, allXYPoints, nPointsPerAxis)
+            TimeUnit.SECONDS.sleep(3)
+            //Get test data, run the test data through the network to generate predictions, and plot those predictions:
+            plotTestData(model, testIter!!, allXYPoints, nPointsPerAxis)
+        }
+    }
+}
